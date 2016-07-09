3dfa1135f29ef29dc5ab567ea4c670d8972eea6d
오준형 <jun@ojunhyeong-ui-MacBook-Pro.local>
2016-07-10 06:37:09 +0900

    login function

--- app/lib/mongodb.js
+++
@@ -1,7 +0,0 @@
-Users = new Meteor.Collection("user");
-if (Meteor.isServer) {
-    Meteor.publish('userData', function () {
-        console.log(newColl.find().fetch());
-        return Users.find();
-    });
-}
